d7f009f7e986e30293fc0fb6c813ae1b63fbcb74
Zharkan <g.j@mailbox.org>
2022-08-22 06:22:15 +0200

    started to write test

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import { CookiesWrapper } from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(
+      <CookiesWrapper doNotTrack="dummy text" trackMe="dummy text" />,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
